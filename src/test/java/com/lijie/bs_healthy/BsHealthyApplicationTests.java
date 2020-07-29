234829e0d1c6d568c9e62ee53e6477bf90b78b9b
lijie <2416856684@qq.com>
2020-07-29 15:00:09 +0800

    启动项

---
+++ src/test/java/com/lijie/bs_healthy/BsHealthyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lijie.bs_healthy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsHealthyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
